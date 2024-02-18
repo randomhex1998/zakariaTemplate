a360358e39c236b0568db5288154e5a9e4dd51d4
randomhex1998 <randomhex1998@gmail.com>
2024-02-18 17:50:13 +0330

    make slider with swiperjs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 let j = $.noConflict();
 
 
-j(document).ready( function (){
-    j(".bars").on("click" , () =>{
+j(document).ready(function () {
+    j(".bars").on("click", () => {
         j(".mobile-menu").toggleClass("active-mobile-menu");
         j(".wrapper").toggleClass("active-mobile-menu");
     })
-    j(".wrapper").on("click" , () =>{
+    j(".wrapper").on("click", () => {
         j(".mobile-menu").removeClass("active-mobile-menu");
         j(".wrapper").removeClass("active-mobile-menu");
     })
+
+
+    //swiper slider
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        },
+        autoplay: {
+            delay: 3000,
+        },
+    });
 })
