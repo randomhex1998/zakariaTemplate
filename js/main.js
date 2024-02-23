dc225647f28bc1f9b9e6101cddd206184263da83
randomhex1998 <randomhex1998@gmail.com>
2024-02-23 10:38:34 +0330

    make laptop section

--- js/main.js
+++ js/main.js
@@ -41,5 +41,41 @@ j(document).ready(function () {
         },
     });
 
+    var swiperLaptop = new Swiper(".swiperLaptop", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 30
+            },
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 40
+            },
+            992:{
+                slidesPerView: 4,
+                spaceBetween: 40
+            },
+
+            1200:{
+                slidesPerView: 5,
+                spaceBetween: 40
+            }
+          }
+      });
+
 
 })
